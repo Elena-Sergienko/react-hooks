585b6cbdc2e04d95ae63110a0eca6125283d3dc9
Elena-Sergienko <elena.serg.us@gmail.com>
2022-02-05 14:14:27 -0800

    add useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import useInput from "./hooks/useInput";
 import Hover from "./components/Hover";
 import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
 
 // https://www.youtube.com/watch?v=ks8oftGP2oc
-// 20:29
 
 function App() {
     const username = useInput('');
     const password = useInput('');
 
+    const [value, setValue] = useState("")
+    const debouncedSearch = useDebounce(search, 500);
+
+    function search (query) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+            })
+    }
+
+    const onChange = e => {
+        setValue(e.target.value);
+        debouncedSearch(e.target.value);
+    }
 
     return (
         <div>
@@ -19,12 +34,14 @@ function App() {
             <button onClick={() => console.log(username, password)}>Click</button>
 
             <hr/>
-
             <h3>useHover</h3>
             <Hover/>
 
             <hr/>
+            <h3>Search</h3>
+            <input type="text" value={value} onChange={onChange}/>
 
+            <hr/>
             <h3>List</h3>
             <List/>
         </div>
